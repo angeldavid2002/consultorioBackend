0f64bcc47e96e27f9ac869849840a40238ac4b4d
STRIKER013 <66685038+STRIKER013@users.noreply.github.com>
2021-11-01 17:25:31 -0500

    Componentes historia
    Creacion de los componentes de historia

---
+++ proyectoConsultorio/ClientApp/src/app/Consultorio/historia/eliminar-historia/eliminar-historia.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-eliminar-historia',
+  templateUrl: './eliminar-historia.component.html',
+  styleUrls: ['./eliminar-historia.component.css']
+})
+export class EliminarHistoriaComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
---
+++ proyectoConsultorio/ClientApp/src/app/Consultorio/historia/registrar-historia/registrar-historia.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-registrar-historia',
+  templateUrl: './registrar-historia.component.html',
+  styleUrls: ['./registrar-historia.component.css']
+})
+export class RegistrarHistoriaComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
